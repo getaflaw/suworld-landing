9ba1b8f93552aa8407f25047dba4feee3a4deb3d
unknown <getaflaw@gmail.com>
2022-04-14 14:49:59 +0300

    fix some

--- static/gBqyTyftlcEgo_U07rOCR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/90-8a801c682b7348c6.js","static/chunks/pages/index-4c194e45222edb70.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
